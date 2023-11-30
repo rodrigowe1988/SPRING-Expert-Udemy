6bdfe9196588595119e79d216cf6ef3e7fe49af2
Weber <rodrigoweber97@hotmail.com>
2023-11-30 17:05:01 -0300

    configurando roles, authorities e urls

--- src/main/java/com/example/vendas/config/SecurityConfig.java
+++ src/main/java/com/example/vendas/config/SecurityConfig.java
@@ -1,11 +1,9 @@
 package com.example.vendas.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -25,11 +24,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .passwordEncoder(passwordEncoder())
                 .withUser("fulano")
                 .password(passwordEncoder().encode("123"))
-                .roles("USER");
+                .roles("USER", "ADMIN");
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+    protected void configure( HttpSecurity http ) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                    .antMatchers("/api/clientes/**")
+                        .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/pedidos/**")
+                        .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/produtos/**")
+                        .hasRole("ADMIN")
+                .and()
+                    .formLogin();
+        ;
     }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,7 +4,7 @@ INSERT INTO clientes (nome, cpf) values ('Aline Maria', '175.605.580-70');
 INSERT INTO clientes (nome, cpf) values ('Rodrigo Weber','383.473.670-86');
 
 
-INSERT INTO pedidos (cliente_id,data_pedido,status,total) values (1, TIMESTAMP WITH TIME ZONE '1988-01-13','REALIZADO',2000.0);
+--INSERT INTO pedidos (cliente_id,data_pedido,status,total) values (1, TIMESTAMP WITH TIME ZONE '1988-01-13','REALIZADO',2000.0);
 
 INSERT INTO produtos (descricao,preco_unitario) values ('TV SAMSUNG 40 polegadas', 2000.0);
 INSERT INTO produtos (descricao,preco_unitario) values ('Macbook PRO M1 MAX', 15000.0);
