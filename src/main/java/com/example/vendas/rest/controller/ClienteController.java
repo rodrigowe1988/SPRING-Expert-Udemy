68362b074b81726efbb84d575591d67db71e5f64
Weber <rodrigoweber97@hotmail.com>
2023-12-11 10:03:37 -0300

    customizando a UI da documentacao da API

--- src/main/java/com/example/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/example/vendas/rest/controller/ClienteController.java
@@ -2,6 +2,7 @@ package com.example.vendas.rest.controller;
 
 import com.example.vendas.entities.Cliente;
 import com.example.vendas.services.ClienteService;
+import io.swagger.annotations.*;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.HttpStatus;
@@ -14,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping(value = "/api/clientes")
+@Api("API Clientes")
 public class ClienteController {
 
     private ClienteService service;
@@ -23,13 +25,19 @@ public class ClienteController {
     }
 
     @GetMapping
+    @ApiOperation("Listar todos os clientes")
     public ResponseEntity<List<Cliente>> findAll() {
         List<Cliente> list = service.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+    @ApiOperation("Obter detalhes de um cliente")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "Cliente encontrado"),
+            @ApiResponse(code = 404, message = "Cliente não encontrado com o ID informado")
+    })
+    public ResponseEntity<Cliente> getClienteById( @PathVariable @ApiParam("Id do cliente") Integer id) {
         Optional<Cliente> cliente = service.findById(id);
         if(cliente.isPresent()) {
             return ResponseEntity.ok(cliente.get());
@@ -38,30 +46,39 @@ public class ClienteController {
     }
 
     @GetMapping("/nome")
+    @ApiOperation(" Buscar cliente por nome")
     public ResponseEntity<List<Cliente>> getClienteByNome(@RequestParam String nome) {
         List<Cliente> cliente = service.findByNome(nome);
         return ResponseEntity.ok().body(cliente);
     }
 
     @PostMapping
+    @ApiOperation("Salva um novo cliente")
+    @ApiResponses({
+            @ApiResponse(code = 201, message = "Cliente criado com sucesso"),
+            @ApiResponse(code = 400, message = "Erro de validação")
+    })
     public ResponseEntity save(@RequestBody @Valid Cliente cliente) {
         service.insert(cliente);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/{id}")
+    @ApiOperation("Atualizar dados de um cliente")
     public ResponseEntity update(@PathVariable Integer id, @RequestBody @Valid Cliente cliente) {
         service.update(id, cliente);
         return ResponseEntity.ok().body(cliente);
     }
 
     @DeleteMapping(value = "/{id}")
+    @ApiOperation("Deletar um cliente")
     public ResponseEntity<Void> deleteById(@PathVariable Integer id) {
         service.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @GetMapping("/busca-geral")
+    @ApiOperation("Buscar cliente por nome e/ou cpf")
     public ResponseEntity find (Cliente filtro) {
         ExampleMatcher matcher = ExampleMatcher
                                     .matching()
